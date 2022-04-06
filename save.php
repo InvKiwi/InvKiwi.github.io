611eb9c90e98c15f9c151f3a3eeaf1f3d6694960
InvKiwi <leon.04.sch@gmail.com>
2022-04-06 20:08:18 +0200

    update zum speichern der nutzereingaben

---
+++ save.php
@@ -0,0 +1,11 @@
+<?php
+    ectract($_REQUEST)
+    $file = fopen("interessenten.txt", "a");
+
+    fwrite($file,"Name: ");
+    fwrite($file,$name "\n");
+    fwrite($file,"E-Mail: ");
+    fwrite($file,$emailadresse "\n");
+    fwrite($file, "\n")
+    fclose($file);
+?>
